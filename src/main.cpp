64a43986cb60296ff44693fc029b85cb50bf28a9
Benjamin Scholar <bens@digitalman.com>
2020-03-14 17:49:59 -0700

    compiles

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include <iostream>
 
 #include "units.h"
+#include "yaml-cpp/parser.h"
 
 TalonSRX talon{1};
 
 int main() {
+  YAML::Parser parser;
   std::cout << "Yeet" << std::endl;
 
   return 0;
